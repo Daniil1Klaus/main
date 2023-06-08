504c7750829f2c662acdb03449fd86eb0581d7eb
Daniil <spatyfylus@mail.ru>
2023-06-08 02:00:14 -0400

    version 2

--- itt.c
+++ itt.c
@@ -5,6 +5,8 @@ int main()
     int a=5;
     int b=3;
     int sum=a+b;
+    int deference = a-b;
     printf("сумма равна %d\n", sum);
+    printf("разность равна %d\n", deference);
     return 0;
 }
